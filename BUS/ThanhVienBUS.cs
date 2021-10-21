7c9bc8eb21176981ef0dfb463083facb4dc00d8f
Nguyen Thi My Linh <91240116+mylinh01@users.noreply.github.com>
2021-10-21 11:43:52 +0700

    update stored procedure

---
+++ BUS/ThanhVienBUS.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BUS
+{
+    public class ThanhVienBUS
+    {
+        DAL_QLCuaHangSach db;
+        public ThanhVienBUS()
+        {
+            db = new DAL_QLCuaHangSach();
+        }
+        public DataSet getThanhVien()
+        {
+            return db.ExecuteQueryDataSet("spGetThanhVien", CommandType.StoredProcedure);
+        }
+
+        public bool insertThanhVien(ref string err,
+            string hoten,
+            string diachi,
+            string sdt,
+            long id_loaitv,
+            DateTime ngaytao,
+            DateTime ngayupdate)
+        {
+            return db.MyExecuteNonQuery("spInsertThanhVien", CommandType.StoredProcedure, ref err,
+                new SqlParameter("@HoTen", hoten),
+                new SqlParameter("@DiaChi", diachi),
+                new SqlParameter("@SDT", sdt),
+                new SqlParameter("@ID_LoaiTV", id_loaitv),
+                new SqlParameter("@NgayTao", ngaytao),
+                new SqlParameter("@NgayUpdate", ngayupdate));
+        }
+        public bool deleteThanhVien(ref string err,
+            string id)
+        {
+            return db.MyExecuteNonQuery("spDeleteThanhVien", CommandType.StoredProcedure, ref err,
+                new SqlParameter("@ID", id));
+        }
+        public bool updateThanhVien(ref string err,
+            long id,
+            string hoten,
+            string diachi,
+            string sdt,
+            long id_loaitv,
+            DateTime ngaytao,
+            DateTime ngayupdate)
+        {
+            return db.MyExecuteNonQuery("spUpdateThanhVien", CommandType.StoredProcedure, ref err,
+                new SqlParameter("@ID", id),
+                new SqlParameter("@HoTen", hoten),
+                new SqlParameter("@DiaChi", diachi),
+                new SqlParameter("@SDT", sdt),
+                new SqlParameter("@ID_LoaiTV", id_loaitv),
+                new SqlParameter("@NgayTao", ngaytao),
+                new SqlParameter("@NgayUpdate", ngayupdate));
+        }
+    }
+}
